ff4293bd17d4794d1d5051e6e0c0e5046f657020
Yves Chedemois <y.chedemois+drupal.org@gmail.com>
2015-12-11 12:48:39 +0100

    easier Immutable syntax

--- client/store/reducers.jsx
+++ client/store/reducers.jsx
@@ -2,7 +2,7 @@ import { createReducer } from 'redux-immutablejs';
 
 export default {
   userInterface: createReducer({selectedId: '', sort: {field: 'index', order: 1}}, {
-    'SELECT_PLAYER': (userInterface, {payload: playerId}) => userInterface.set('selectedId', playerId),
-    'SET_SORTING': (userInterface, {payload: {field, order}}) => userInterface.setIn(['sort', 'field'], field).setIn(['sort', 'order'], order)
+    'SELECT_PLAYER': (state, {payload: playerId}) => state.merge({selectedId: playerId}),
+    'SET_SORTING': (state, {payload: {field, order}}) => state.merge({sort: {field, order}})
   }),
 }
