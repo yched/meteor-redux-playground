70faef222f0b3b4e148a840f28b6c33626fe77eb
Yves Chedemois <y.chedemois+drupal.org@gmail.com>
2015-12-15 23:34:26 +0100

    move Meteor tracking to App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { compose, pure, setPropTypes } from 'recompose';
+import { pure, setPropTypes } from 'recompose';
 import { mapOf } from 'react-immutable-proptypes';
 import { playerPropType } from 'client/immutable_models/player';
 import { DragDropContext } from 'react-dnd';
@@ -7,59 +7,91 @@ import HTML5Backend from 'react-dnd-html5-backend';
 import PlayerList from './PlayerList';
 import SelectPlayer from './SelectPlayer';
 import { Link } from 'react-router'
+import Players from 'both/models/player';
 
-const App = props => (
-  <div className="outer">
-    <div className="logo"></div>
-    <h1 className="title">Leaderboard</h1>
-    <div className="subtitle">Select a scientist to give them points</div>
+@DragDropContext(HTML5Backend)
+@pure
+@setPropTypes({
+  listId: React.PropTypes.number.isRequired,
+  players: mapOf(playerPropType).isRequired,
+  playerView: React.PropTypes.string.isRequired,
+  selectedPlayer: playerPropType,
 
-    <input type="radio" name="sorting" defaultChecked={props.playerView === 'by_index'} onClick={() => props.actions.setPlayerView('by_index')} />
-    Manual sort
-    <input type="radio" name="sorting" defaultChecked={props.playerView === 'by_score'} onClick={() => props.actions.setPlayerView('by_score')} />
-    Sort by scores
+  actions: React.PropTypes.shape({
+    selectPlayer: React.PropTypes.func.isRequired,
+    setPlayerView: React.PropTypes.func.isRequired,
+    incrementPlayerScore: React.PropTypes.func.isRequired,
+    updatePlayerIndexes: React.PropTypes.func.isRequired
+  }).isRequired
+})
+class App extends React.Component {
 
-    <div>
-      <PlayerList players={props.players}
-                  selectedPlayer={props.selectedPlayer}
-                  playerView={props.playerView}
-                  selectPlayer={props.actions.selectPlayer}
-                  updatePlayerIndexes={props.actions.updatePlayerIndexes} />
-    </div>
+  // Subscribe to the Players publication, and track reactive changes.
+  componentWillMount() {
+    this._subscribeToPlayers(this.props.playerView, {listId: this.props.listId});
+  }
 
-    <SelectPlayer selectedPlayer={props.selectedPlayer}
-                  incrementPlayerScore={props.actions.incrementPlayerScore} />
+  // Re-subscribe if the view changed.
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.playerView !== this.props.playerView || nextProps.listId !== this.props.listId) {
+      this._subscribeToPlayers(nextProps.playerView, {listId: nextProps.listId});
+    }
+  }
 
-    <ul>
-      {/* use Link to route around the app */}
-      {[1, 2].map(listId => (
-        <li key={listId}>
-          {listId === props.listId ?
-            <div>List {listId}</div> :
-            <Link to={`/list/${listId}`}>List {listId}</Link>
-          }
-        </li>
-      ))}
-    </ul>
+  _subscribeToPlayers(viewName, params) {
+    // Subscribe to 'players' publication, with the current view.
+    this.sub = Meteor.subscribe('players', viewName, params);
+    // Track the Minimongo cursor on the request we're interested in.
+    this.track && this.track.stop();
+    this.track = this.props.actions.trackPlayerCollection(Players.getCursor(viewName, params));
+  }
 
-  </div>
-);
+  componentWillUnmount() {
+    // Unsubscribe to 'players' publication.
+    this.sub.stop();
+    // Stop tracking the request.
+    this.track.stop();
+  }
 
-export default compose(
-  DragDropContext(HTML5Backend),
-  pure,
-  setPropTypes({
-    listId: React.PropTypes.number.isRequired,
-    players: mapOf(playerPropType).isRequired,
-    playerView: React.PropTypes.string.isRequired,
-    selectedPlayer: playerPropType,
+  render() {
+    const props = this.props;
+    return (
+      <div className="outer">
+        <div className="logo"></div>
+        <h1 className="title">Leaderboard</h1>
+        <div className="subtitle">Select a scientist to give them points</div>
 
-    actions: React.PropTypes.shape({
-      selectPlayer: React.PropTypes.func.isRequired,
-      setPlayerView: React.PropTypes.func.isRequired,
-      incrementPlayerScore: React.PropTypes.func.isRequired,
-      updatePlayerIndexes: React.PropTypes.func.isRequired
-    }).isRequired
-  })
-)(App);
+        <input type="radio" name="sorting" defaultChecked={props.playerView === 'by_index'} onClick={() => props.actions.setPlayerView('by_index')} />
+        Manual sort
+        <input type="radio" name="sorting" defaultChecked={props.playerView === 'by_score'} onClick={() => props.actions.setPlayerView('by_score')} />
+        Sort by scores
+
+        <div>
+          <PlayerList players={props.players}
+                      selectedPlayer={props.selectedPlayer}
+                      playerView={props.playerView}
+                      selectPlayer={props.actions.selectPlayer}
+                      updatePlayerIndexes={props.actions.updatePlayerIndexes} />
+        </div>
+
+        <SelectPlayer selectedPlayer={props.selectedPlayer}
+                      incrementPlayerScore={props.actions.incrementPlayerScore} />
+
+        <ul>
+          {[1, 2].map(listId => (
+            <li key={listId}>
+              {listId !== props.listId ?
+                <Link to={`/list/${listId}`}>List {listId}</Link> :
+                <div>List {listId}</div>
+              }
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    )
+  }
+}
+
+export default App;
 
--- client/containers/AppContainer.jsx
+++ client/containers/AppContainer.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import actions from 'client/store/actions';
 import * as selectors from '../selectors/index';
-import Players from 'both/models/player';
 import App from 'client/components/App';
 
 // Connect to the redux store :
@@ -23,33 +22,6 @@ const mapDispatchToProps = (dispatch) => ({
 
 @connect(mapStateToProps, mapDispatchToProps)
 class AppContainer extends React.Component {
-  componentWillMount() {
-    // Subscribe to the Players publication, and track reactive changes.
-    this._subscribeToPlayers(this.props.playerView, {listId: this.props.params.listId});
-  }
-
-  componentWillReceiveProps(nextProps) {
-    // Re-subscribe if the view changed.
-    if (nextProps.playerView !== this.props.playerView || nextProps.params.listId !== this.props.params.listId) {
-      this._subscribeToPlayers(nextProps.playerView, {listId: nextProps.params.listId});
-    }
-  }
-
-  _subscribeToPlayers(viewName, params) {
-    // Subscribe to 'players' publication, with the current view.
-    this.sub = Meteor.subscribe('players', viewName, params);
-    // Track the Minimongo cursor on the request we're interested in.
-    this.track && this.track.stop();
-    this.track = this.props.actions.trackPlayerCollection(Players.getCursor(viewName, params));
-  }
-
-  componentWillUnmount() {
-    // Unsubscribe to 'players' publication.
-    this.sub.stop();
-    // Stop tracking the request.
-    this.track.stop();
-  }
-
   render() {
     const appProps = {...this.props, listId: parseInt(this.props.params.listId)};
     return (
