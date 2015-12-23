58a74e72b83ed8022ba9ab7694ba290be11f1204
Yves Chedemois <y.chedemois+drupal.org@gmail.com>
2015-12-23 15:18:38 +0100

    minor

--- server/main.jsx
+++ server/main.jsx
@@ -1,3 +1,9 @@
+// Temporary fix so that externals defined by the various meteor extensions
+// (like Promise) are available in the global space.
+// See https://github.com/thereactivestack/meteor-webpack/issues/38#issuecomment-166626296
+import Symbol from 'symbol';
+global.Symbol = Symbol;
+
 import 'both/models/methods'
 import 'server/publications'
 import 'server/fixtures'
@@ -6,12 +12,6 @@ import * as settings from 'settings.jsx'
 console.log("Starting Leaderboard Server...");
 
 // @todo see https://github.com/thereactivestack/kickstart-hugeapp
-
-// Temporary fix so that externals defined by the various meteor extensions
-// (like Promise) are available in the global space.
-// See https://github.com/thereactivestack/meteor-webpack/issues/38#issuecomment-166626296
-import Symbol from 'symbol';
-global.Symbol = Symbol;
 
 // Import what's needed for Server-Side Rendering.
 import ReactRouterSSR from './ssr.jsx'
@@ -23,27 +23,7 @@ import Root from 'client/components/Root'
 if (settings.ssr) {
   console.log('SSR !!');
 
-// @todo
-// http://rackt.org/redux/docs/recipes/ServerRendering.html
-// You may want to read Async Actions to learn more about expressing asynchronous
-// flow in Redux with async primitives such as Promises and thunks. Keep in mind that
-// anything you learn there can also be applied to universal rendering.
-
-// If you use something like React Router, you might also want to express your
-// data fetching dependencies as static fetchData() methods on your route handler components.
-// They may return async actions, so that your handleRender function can match
-// the route to the route handler component classes, dispatch fetchData() result
-// for each of them, and render only after the Promises have resolved. This way
-// the specific API calls required for different routes are colocated with the
-// route handler component definitions. You can also use the same technique on
-// the client side to prevent the router from switching the page until its data has been loaded.
-
-// see https://github.com/jlongster/react-redux-universal-hot-example/commit/e13b93518c8c9b5524ad50f1c566ed6c480c1888?diff=split
-//   + https://github.com/jlongster/react-redux-universal-hot-example/pull/6
-
   Meteor.startup(() => {
-
-    // Do the rendering.
     ReactRouterSSR.Run(routes, {}, {
       wrapper: Root,
       createReduxStore: (history, initialState) => {
