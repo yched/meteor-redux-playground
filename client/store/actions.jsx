307698fb89b2d54303f3eb79095d40c9d82a0b5d
Yves Chedemois <y.chedemois+drupal.org@gmail.com>
2015-12-27 15:30:43 +0100

    alternative async with thunks

--- client/store/actions.jsx
+++ client/store/actions.jsx
@@ -11,18 +11,32 @@ export default {
   selectPlayer: createAction('SELECT_PLAYER', playerId => playerId),
   setPlayerView: createAction('SET_PLAYER_VIEW', viewName => viewName),
 
-  // Optimistic async action: dispatch 2 actions (redux-multi)
-  fetchHttp: (url) => [
-    // - a regular 'STARTED' action,
-    {type: 'FETCH_HTTP_STARTED', payload: url},
-    // - an async action (redux-promise), with a Promise as payload
-    {type: 'FETCH_HTTP_FINISHED', payload: fetch(url).then(response => response.text())},
-    // Alternate, more explicit : dispatch the Promise directly, handle error ourselves.
-    //fetch(url)
-    //  .then(response => response.text())
-    //  .then(content => ({type: 'FETCH_HTTP_FINISHED', payload: content}))
-    //  .catch(err => ({type: 'FETCH_HTTP_FINISHED', payload: err,  error: true}))
-  ],
+  // Optimistic async action: with redux-multi and redux-promise
+  // Dispatch 2 actions (redux-multi) :
+  //fetchHttp: url => [
+  //  // - a regular 'STARTED' action,
+  //  {type: 'FETCH_HTTP_STARTED', payload: url},
+  //  // - an async action (redux-promise), with a Promise as payload
+  //  {type: 'FETCH_HTTP_FINISHED', payload: fetch(url).then(response => response.text())},
+  //  // Alternate, more explicit : redux-promise allows to dispatch the Promise directly, but we need to
+  //  // handle error explicitly.
+  //  //fetch(url)
+  //  //  .then(response => response.text())
+  //  //  .then(content => ({type: 'FETCH_HTTP_FINISHED', payload: content}))
+  //  //  .catch(err => ({type: 'FETCH_HTTP_FINISHED', payload: err, error: true}))
+  //],
+
+  // Alternative with just redux-thunk :
+  fetchHttp: url =>
+    dispatch => {
+      const fetchHttpStarted = createAction('FETCH_HTTP_STARTED', url => url);
+      const fetchHttpFinished = createAction('FETCH_HTTP_FINISHED', content => content);
+      dispatch(fetchHttpStarted(url));
+      fetch(url)
+        .then(response => response.text())
+        .then(content => dispatch(fetchHttpFinished(content)))
+        .catch(err => dispatch(fetchHttpFinished(err)));
+    },
 
   // Side effect (Meteor method call) via redux-thunk
   incrementPlayerScore: (playerId, increment, callback) =>
