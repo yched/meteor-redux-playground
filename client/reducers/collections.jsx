8d98ed79903c3ff15993f24395c0f47038a1b1c2
Yves Chedemois <y.chedemois+drupal.org@gmail.com>
2016-01-01 23:34:05 +0100

    simplify doc merge logic

--- client/reducers/collections.jsx
+++ client/reducers/collections.jsx
@@ -24,18 +24,18 @@ function collectionReducer(collection) {
 
 // Build an immutable map of records, trying to keep existing ones from a
 // previous map.
-function immutableCollectionConstructor(recordClass, docs, prev) {
+function immutableCollectionConstructor(recordClass, docs, prevMap) {
   if (!docs.length) {
     return Immutable.Map();
   }
 
   let newMap;
 
-  // If possible, start from the previous map, and remove docs that are not in
-  // the new data.
-  if (prev) {
-    newMap = prev;
-    const prevIds = prev.keySeq().toJS();
+  // If possible, start from the previous map by removing docs that are not in
+  // the new data anymore.
+  if (prevMap) {
+    newMap = prevMap;
+    const prevIds = prevMap.keySeq().toJS();
     const newIds = _.pluck(docs, '_id');
     _.difference(prevIds, newIds).map(id => {
       newMap = newMap.remove(id);
@@ -44,19 +44,13 @@ function immutableCollectionConstructor(recordClass, docs, prev) {
     newMap = Immutable.Map();
   }
 
-  // Add the new docs, keeping records if they exist.
+  // Add the new docs.
   docs.map(doc => {
-    let record;
-    if (prev && prev.has(doc._id)) {
-      record = prev.get(doc._id);
-      // Note: Record.merge() changes the nature of the content of the record.
-      // Manually set all properties instead...
-      Object.keys(doc).map(key => {
-        record = record.set(key, doc[key]);
-      });
-    }
-    else {
-      record = new recordClass(doc);
+    let record = new recordClass(doc);
+    // If a previous record existed for the _id, merge the new record into that
+    // one, which preserves identity if the doc content didn't change.
+    if (prevMap && prevMap.has(doc._id)) {
+      record = prevMap.get(doc._id).merge(record);
     }
     newMap = newMap.set(doc._id, record);
   });
